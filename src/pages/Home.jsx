9276d5dd71aaeee6cd6457502ed1bc531df42a0c
Ansari Afroz Ahmed <ansariafroz.py@gmail.com>
2023-09-27 22:16:34 +0530

    Top Characters

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import BannerSlides from '../components/BannerSlides'
 import TopAiring from '../components/TopAiring/TopAiring'
+import TopAnimeChar from '../components/TopAnimeCharacters/TopAnimeChar'
 
 const Home = () => {
     return (
         <div className='pt-5 lg:pt-0 '>
             <BannerSlides />
             <TopAiring />
+            <TopAnimeChar />
         </div>
     )
 }
