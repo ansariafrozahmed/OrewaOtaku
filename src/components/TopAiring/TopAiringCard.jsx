bbc6e79fc66b367077fe77bcacb4c7ed047d58f1
Ansari Afroz Ahmed <ansariafroz.py@gmail.com>
2023-10-01 21:28:28 +0530

    Last Changed Of The Day

--- src/components/TopAiring/TopAiringCard.jsx
+++ src/components/TopAiring/TopAiringCard.jsx
@@ -7,8 +7,8 @@ const TopAiringCard = ({ image, name, type, rank }) => {
                 className="w-full h-[250px] lg:h-[330px] bg-cover "
                 style={{ backgroundImage: `url(${image.webp.large_image_url})` }}
             >
-                <div class="absolute -left-8 -top-8 h-16 w-16">
-                    <div class="absolute transform -rotate-45 font-sans border-b-4 border-black bg-[#FF0000] text-center shadow-xl text-[1.3rem] text-white font-semibold left-[-34px] top-[32px] w-[170px]">
+                <div className="absolute -left-10 lg:-left-10 -top-8 h-16 w-16">
+                    <div className="absolute transform -rotate-45 font-sans border-b-4 border-black bg-[#FF0000] text-center shadow-xl text-[0.9rem] text-white font-semibold left-[-34px] top-[32px] w-[170px]">
                         {'#' + rank}
                     </div>
                 </div>
