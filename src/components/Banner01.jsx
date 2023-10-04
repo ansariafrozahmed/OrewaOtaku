5e1a0b28f315a5ac4420866e6aa689b163e07d51
Ansari Afroz Ahmed <ansariafroz.py@gmail.com>
2023-10-04 22:52:01 +0530

    Updated

--- src/components/Banner01.jsx
+++ src/components/Banner01.jsx
@@ -1,17 +1,20 @@
 import React from "react";
 
 const Banner01 = () => {
-    const imagelink = "https://images7.alphacoders.com/132/1325984.jpeg";
+    const imagelink = "https://static.crunchyroll.com/fms/landscape_large/94/png/a5bac8ac-8bf6-4a78-9674-4730fac66ab6.webp";
 
     return (
         <div
-            className=" h-[500px] lg:h-[500px] overflow-hidden w-full bg-center bg-cover"
+            className=" h-[200px] lg:h-[80vh] overflow-hidden bg-no-repeat w-full bg-center-left bg-cover mt-5"
             style={{ backgroundImage: `url(${imagelink})` }}
         >
-            <div className="pb-5 h-full w-full bg-gradient-to-t from-slate-300 flex items-end justify-center">
+            {/* <div className="pb-5 h-full w-full bg-gradient-to-t from-slate-300 flex items-end justify-center">
                 <span className="animatedtext uppercase text-[15vw] text-center lg:text-9xl font-black ">
                     Are You An Otaku ?
                 </span>
+            </div> */}
+            <div className="h-full w-[35%] flex items-center justify-start px-2 lg:px-10">
+                <p className=" text-[1.55rem] lg:text-[5.5rem] font-bold uppercase leading-none ">Orewa<br /><span className="text-[1.7rem] lg:text-[6rem]">Otaku</span></p>
             </div>
         </div>
     );
--- src/components/HeaderFooter/Footer.jsx
+++ src/components/HeaderFooter/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <div className='h-[50vh] w-full flex items-center justify-center bg-center bg-cover' style={{ backgroundImage: `url(OrewaOtakuFooterBg.jpg)` }}>
+        <div className='h-[20vh] w-full flex items-center px-2 justify-center bg-center bg-cover' style={{ backgroundImage: `url(OrewaOtakuFooterBg.jpg)` }}>
             <p className='text-center animatedtext font-bold text-[0.9rem] tracking-wider'>© OrewaOtaku All rights reserved || Developed By Ansari Afroz Ahmed</p>
         </div>
     )
