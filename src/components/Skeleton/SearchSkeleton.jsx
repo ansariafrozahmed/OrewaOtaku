24186fad4abd0361342c0f91b923200ba67d6236
Ansari Afroz Ahmed <ansariafroz.py@gmail.com>
2023-10-15 20:35:59 +0530

    Updated

--- src/components/Skeleton/SearchSkeleton.jsx
+++ src/components/Skeleton/SearchSkeleton.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Skeleton from 'react-loading-skeleton'
 const SearchSkeleton = () => {
     return (
-        <div className='w-full flex gap-2'>
+        <div className='w-full flex gap-2 mt-5'>
             <div className='h-full w-full aspect-[3/4] flex flex-col gap-1'>
                 <Skeleton className='aspect-[3/4] h-full w-full' />
                 <Skeleton className='' />
