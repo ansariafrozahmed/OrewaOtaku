9e59aa88269e4aba46f82e99cc05a00551dabfb7
Ansari Afroz Ahmed <ansariafroz.py@gmail.com>
2023-10-19 23:06:13 +0530

    Updated

--- src/pages/Genre.jsx
+++ src/pages/Genre.jsx
@@ -1,60 +1,75 @@
-import React from 'react'
-import { motion } from 'framer-motion'
-import { GiPunchBlast } from 'react-icons/gi'
+import React from "react";
+import { motion } from "framer-motion";
+import {
+    GiPunchBlast,
+    GiDramaMasks,
+    GiSelfLove,
+    GiMaterialsScience,
+    GiLoveMystery,
+    GiPowerLightning,
+} from "react-icons/gi";
+import { RiTreasureMapLine } from "react-icons/ri";
+import { FaRegLaughSquint } from "react-icons/fa";
 
 const Genre = () => {
-
     const category = [
         {
             id: 1,
-            icon: '',
-            label: 'Action',
-            image: 'https://wallpapers.com/images/hd/action-anime-1gwafa8zya1v1tsz.jpg',
+            icon: <GiPunchBlast size={30} />,
+            label: "Action",
+            image:
+                "https://wallpapers.com/images/high/action-anime-cd1acx54k1gly3qj.webp",
         },
         {
             id: 2,
-            icon: '',
-            label: 'Action',
-            image: 'https://wallpapers.com/images/hd/action-anime-1gwafa8zya1v1tsz.jpg',
+            icon: <RiTreasureMapLine size={30} />,
+            label: "Adventure",
+            image:
+                "https://wallpapers.com/images/high/digimon-background-atdsmlsttaro9n69.webp",
         },
         {
             id: 3,
-            icon: '',
-            label: 'Action',
-            image: 'https://wallpapers.com/images/hd/action-anime-1gwafa8zya1v1tsz.jpg',
+            icon: <FaRegLaughSquint size={30} />,
+            label: "Comedy",
+            image:
+                "https://wallpapers.com/images/high/funny-anime-one-piece-artwork-xutitmdmtgfzwhpl.webp",
         },
         {
             id: 4,
-            icon: '',
-            label: 'Action',
-            image: 'https://wallpapers.com/images/hd/action-anime-1gwafa8zya1v1tsz.jpg',
+            icon: <GiDramaMasks size={30} />,
+            label: "Drama",
+            image:
+                "https://wallpapers.com/images/high/k-on-background-spmp1zaxd4dm10ve.webp",
         },
         {
             id: 5,
-            icon: '',
-            label: 'Action',
-            image: 'https://wallpapers.com/images/hd/action-anime-1gwafa8zya1v1tsz.jpg',
+            icon: <GiSelfLove size={30} />,
+            label: "Fantasy",
+            image:
+                "https://wallpapers.com/images/high/anime-love-city-night-lights-4ld79mel973e9plp.webp",
         },
         {
             id: 6,
-            icon: '',
-            label: 'Action',
-            image: 'https://wallpapers.com/images/hd/action-anime-1gwafa8zya1v1tsz.jpg',
+            icon: <GiMaterialsScience size={30} />,
+            label: "Sci-fi",
+            image:
+                "https://wallpapers.com/images/high/gundam-1920-x-1080-background-5fnt5kmwwfs3quxc.webp",
         },
         {
             id: 7,
-            icon: '',
-            label: 'Action',
-            image: 'https://wallpapers.com/images/hd/action-anime-1gwafa8zya1v1tsz.jpg',
+            icon: <GiLoveMystery size={30} />,
+            label: "Romance",
+            image:
+                "https://wallpapers.com/images/high/student-couple-with-sunlight-romance-anime-4c2a3s7adq0vjvun.webp",
         },
         {
             id: 8,
-            icon: '',
-            label: 'Action',
-            image: 'https://wallpapers.com/images/hd/action-anime-1gwafa8zya1v1tsz.jpg',
+            icon: <GiPowerLightning size={30} />,
+            label: "Shounen",
+            image:
+                "https://wallpapers.com/images/hd/one-piece-luffy-holding-hat-nazsl5hmss8bfb9n.webp",
         },
-
-    ]
+    ];
 
     return (
         <motion.div
@@ -62,31 +77,37 @@ const Genre = () => {
             animate={{ scale: 1, opacity: 1 }}
             // exit={{ scale: 0.5, opacity: 0 }}
             transition={{ type: "spring", duration: 0.5 }}
-            className='w-[100%] mt-12 px-2 lg:px-20'
+            className="w-[100%] mt-8 lg:mt-12 px-2 lg:px-20"
         >
-
-            <div className='leading-tight pb-5 lg:pb-10'>
-                <h1 className='text-[1.6rem] lg:text-[2.2rem] font-bold tracking-wide uppercase text-[#FFDD95]'>Popular Categories</h1>
-                <p className='text-[0.9rem] lg:text-[1rem] tracking-wider'>Check Out Some Popular Categories Among Anime Fans</p>
+            <div className="leading-tight pb-8 lg:pb-10">
+                <h1 className="text-[1.6rem] lg:text-[2.2rem] font-bold tracking-wide uppercase text-[#FFDD95]">
+                    Popular Categories
+                </h1>
+                <p className="text-[0.9rem] lg:text-[1rem] tracking-wider">
+                    Check Out Some Popular Categories Among Anime Fans
+                </p>
             </div>
 
-            <div className='flex flex-wrap justify-start gap-[2%] '>
-                {
-                    category?.map((item, index) => {
-                        return (
-                            <div key={index} className='aspect-[6/3.5] mb-[2%] w-[48%] lg:w-[23%] bg-cover rounded-lg overflow-hidden transition-all ease-in-out duration-500 active:scale-90 cursor-pointer hover:scale-105' style={{ backgroundImage: `url(${item.image})` }}>
-                                <div className='bg-gradient-to-t from-[#212121] h-full w-full flex flex-col items-center justify-center '>
-                                    <GiPunchBlast size={40} />
-                                    <img src="" alt="" />
-                                    <p className='text-[1.1rem] tracking-wider font-bold uppercase'>{item.label}</p>
-                                </div>
+            <div className="flex flex-wrap justify-start gap-[2%] ">
+                {category?.map((item, index) => {
+                    return (
+                        <div
+                            key={index}
+                            className="aspect-[6/4] mb-[2%] w-[48%] lg:w-[23%] bg-cover rounded-lg overflow-hidden transition-all ease-in-out duration-500 active:scale-90 cursor-pointer hover:scale-105"
+                            style={{ backgroundImage: `url(${item.image})` }}
+                        >
+                            <div className="bg-gradient-to-t from-[#212121] h-full w-full flex flex-col items-center justify-center ">
+                                {item.icon}
+                                <p className="text-[1rem] tracking-wider font-bold uppercase">
+                                    {item.label}
+                                </p>
                             </div>
-                        )
-                    })
-                }
+                        </div>
+                    );
+                })}
             </div>
         </motion.div>
-    )
-}
+    );
+};
 
-export default Genre
+export default Genre;
